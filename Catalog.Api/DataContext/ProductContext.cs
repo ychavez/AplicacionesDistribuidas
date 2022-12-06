ff6613e6c4e9a8e8fa2d415ea516ffee25556ea2
Yael Chavez <you400@gmail.com>
2022-12-05 18:58:18 -0600

    Catalog 1

---
+++ Catalog.Api/DataContext/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Api.DataContext
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
