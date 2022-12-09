2b4cb631d7a2112b9e718a2781c3590bd2b63258
Yael Chavez <you400@gmail.com>
2022-12-08 18:42:05 -0600

    Ordering 1

---
+++ Ordering.Infraestructure/Persistence/OrderContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+
+
+namespace Ordering.Infraestructure.Persistence;
+
+public class OrderContext : DbContext
+{
+    public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Order> Orders { get; set; }
+
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entry in ChangeTracker.Entries<EntityBase>() )
+        {
+            
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedBy = "User";
+                    entry.Entity.CreatedDate = DateTime.UtcNow;
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.LastModifiedBy = "User";
+                    entry.Entity.LastModifiedDate = DateTime.UtcNow;
+                    break;
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
+
---
+++ Ordering.Infraestructure/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Infraestructure.Persistence;
+
+namespace Ordering.Infraestructure.Repositories;
+
+public class GenericRepository<T> where T : EntityBase
+{
+    private readonly OrderContext _orderContext;
+
+
+    public GenericRepository(OrderContext orderContext)
+    {
+        _orderContext = orderContext;
+    }
+
+    /// <summary>
+    /// Nos trae toda la tabla completa
+    /// </summary>
+    /// <returns></returns>
+    public async Task<IReadOnlyList<T>> GetAllAsync()
+        => await _orderContext.Set<T>().ToListAsync();
+
+    /// <summary>
+    /// Nos trae toda la tabla haciendo un filtrado en el parametro Predicate
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    public async Task<IReadOnlyList<T>> GetAllAsync(Expression<Func<T, bool>> predicate)
+     => await _orderContext.Set<T>().Where(predicate).ToListAsync();
+
+
+    public async Task<IReadOnlyList<T>> GetAllAsync(int offset, int limit, Expression<Func<T, bool>>? predicate,
+        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy, params string[] includeStrings)
+    {
+
+    }
+
+
+}
+
