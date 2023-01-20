8960c4f7e460139420f9b16c69bf71f4c64b71c1
Yael Chavez <you400@gmail.com>
2023-01-19 19:58:04 -0600

    sqlite 1

--- DWShop.App/Context/DataContext.cs
+++ DWShop.App/Context/DataContext.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DWShop.App.Models;
+using SQLite;
 
 namespace DWShop.App.Context
 {
-    internal class DataContext
+    public class DataContext
     {
+
+        SQLiteAsyncConnection db;
+
+        async Task Init()
+        {
+
+            if (db is not null)
+                return;
+
+            db = new SQLiteAsyncConnection(Path.Combine(FileSystem.AppDataDirectory, "DWShop.db3")
+                , SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
+
+            await db.CreateTableAsync<Product>();
+        }
+
+
+        public async Task<List<Product>> GetFavorites()
+        {
+            await Init();
+            return await db.Table<Product>().ToListAsync();
+
+
+        }
+
+        public async Task<bool> SetFavorite(Product product)
+        {
+            await Init();
+            var _product = await GetProductById(product.Id);
+
+            if (_product is not null)
+                return false;
+
+            await db.InsertAsync(product);
+
+            return true;
+        }
+
+        async Task<Product> GetProductById(int id)
+        {
+            await Init();
+            return await db.Table<Product>().Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+
     }
 }
