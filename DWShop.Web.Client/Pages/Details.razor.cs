94576fe881b8f70ce7788e2a858b42f6cb8e7214
Yael Chavez <you400@gmail.com>
2023-02-14 21:55:59 -0600

    product detail

---
+++ DWShop.Web.Client/Pages/Details.razor.cs
@@ -0,0 +1,17 @@
+﻿using DWShop.Web.Client.Services.Contracts;
+using Microsoft.AspNetCore.Components;
+
+namespace DWShop.Web.Client.Pages
+{
+    public partial class Details
+    {
+        [Inject]
+        public IProductService ProductService { get; set; } = null!;
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = null!;
+
+        [Parameter]
+        public string ProductId { get; set; }
+       
+    }
+}
